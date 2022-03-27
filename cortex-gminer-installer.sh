bea54b4a0ccbaca1be86af8d60a68760f8994b30
David Surprenant <ds@davidsurprenant.com>
2022-03-27 12:47:01 -0400

    correct typo

--- cortex-gminer-installer.sh
+++ cortex-gminer-installer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gminer-setup.sh CORTEX cortex Gminer gminer cortex ctxc.2miners.com:2222 0x825e53fcb57d2caa791ebf0bb4c2a64773ee1f1b
+./gminer-setup.sh CORTEX cortex Gminer gminer cortex ctxc.2miners.com:2222 0x825e53fcb57d2caa791ebf0bb4c2a64773ee1f1b
--- gminer-setup.sh
+++ gminer-setup.sh
@@ -10,13 +10,13 @@ name=${name:-ViveLeQuebec}
 read -p "port [4067]: " ipBind
 ipBind=${ipBind:-4067}
 
-/software/$4.sh
+./software/$4.sh
 
-sudo echo "#!/bin/bash
+echo "#!/bin/bash
 /usr/bin/$4/miner --algo $5 --server $poolServer --user $wallet.$name --api $ipBind
 " > /usr/bin/$2-$4.sh
 chmod +x /usr/bin/$2-$4.sh
 
-/service.sh $1 $2 $3 $4  
+./service.sh $1 $2 $3 $4  
 
-sudo systemctl daemon-reload
+systemctl daemon-reload
