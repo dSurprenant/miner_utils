07fbd8112c40621a8de4fc55e6aaaa2f7c51c7d1
David Surprenant <ds@davidsurprenant.com>
2022-03-27 12:50:44 -0400

    add permission

--- gminer-setup.sh
+++ gminer-setup.sh
@@ -10,13 +10,13 @@ name=${name:-ViveLeQuebec}
 read -p "port [4067]: " ipBind
 ipBind=${ipBind:-4067}
 
-./software/$4.sh
+sudo ./software/$4.sh
 
-echo "#!/bin/bash
+sudo echo "#!/bin/bash
 /usr/bin/$4/miner --algo $5 --server $poolServer --user $wallet.$name --api $ipBind
 " > /usr/bin/$2-$4.sh
 chmod +x /usr/bin/$2-$4.sh
 
-./service.sh $1 $2 $3 $4  
+sudo ./service.sh $1 $2 $3 $4  
 
-systemctl daemon-reload
+sudo systemctl daemon-reload
